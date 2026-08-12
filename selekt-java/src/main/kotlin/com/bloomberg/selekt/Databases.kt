6d8ac8bbb4851580ac4c1f6282f5c51e5150715c
Ken Shackleton <kshackleton1@bloomberg.net>
2026-08-12 10:16:06 +0100

    Provide reusable blob streaming bindings from selekt-java.Database
    Signed-off-by: Ken Shackleton <kshackleton1@bloomberg.net>

--- selekt-java/src/main/kotlin/com/bloomberg/selekt/Databases.kt
+++ selekt-java/src/main/kotlin/com/bloomberg/selekt/Databases.kt
@@ -384,6 +384,15 @@ class SQLDatabase(
         }
     }
 
+    fun readFromBlob(
+        table: String,
+        column: String,
+        row: Long,
+        offset: Int,
+        limit: Int,
+        stream: OutputStream
+    ) = readFromBlob("main", table, column, row, offset, limit, stream)
+
     @Suppress("Detekt.LongParameterList")
     fun readFromBlob(
         name: String,
@@ -404,6 +413,12 @@ class SQLDatabase(
     }
 
     override fun setTransactionSuccessful() = pledge { session().setTransactionSuccessful() }
+
+    fun sizeOfBlob(
+        table: String,
+        column: String,
+        row: Long
+    ) = sizeOfBlob("main", table, column, row)
 
     fun sizeOfBlob(
         name: String,
@@ -563,6 +578,14 @@ class SQLDatabase(
                 EMPTY_ARRAY
             )
         }
+
+    fun writeToBlob(
+        table: String,
+        column: String,
+        row: Long,
+        offset: Int,
+        stream: InputStream
+    ) = writeToBlob("main", table, column, row, offset, stream)
 
     @Suppress("Detekt.LongParameterList")
     fun writeToBlob(
